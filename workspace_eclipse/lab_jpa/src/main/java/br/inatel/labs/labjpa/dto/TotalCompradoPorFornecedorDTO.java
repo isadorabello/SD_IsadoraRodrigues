cead28a8a29e4a17fc8bf0749e885ccffd5dca31
isadorabello <isadora.bello@gec.inatel.br>
2023-04-18 21:35:45 -0300

    feat: adding DTO class

---
+++ workspace_eclipse/lab_jpa/src/main/java/br/inatel/labs/labjpa/dto/TotalCompradoPorFornecedorDTO.java
@@ -0,0 +1,25 @@
+package br.inatel.labs.labjpa.dto;
+
+import java.math.BigDecimal;
+
+public class TotalCompradoPorFornecedorDTO {
+	
+	private String fornecedorRazaoSocial;
+	private BigDecimal totalComprado;
+	
+	public TotalCompradoPorFornecedorDTO(String fornecedorRazaoSocial, BigDecimal totalComprado) {
+		super();
+		this.fornecedorRazaoSocial = fornecedorRazaoSocial;
+		this.totalComprado = totalComprado;
+	}
+
+	public String getFornecedorRazaoSocial() {
+		return fornecedorRazaoSocial;
+	}
+
+	public BigDecimal getTotalComprado() {
+		return totalComprado;
+	}
+
+
+}
---
+++ workspace_eclipse/lab_jpa/src/main/java/br/inatel/labs/labjpa/service/RelatorioService.java
@@ -0,0 +1,30 @@
+package br.inatel.labs.labjpa.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import br.inatel.labs.labjpa.dto.TotalCompradoPorFornecedorDTO;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+
+@Service
+@Transactional
+public class RelatorioService {
+	
+	@PersistenceContext
+	private EntityManager em;
+	
+	public List<TotalCompradoPorFornecedorDTO> pesquisarTotalCompradoPorFornecedor(){
+		String query = "select new br.inatel.labs.labjpa.dto.TotalCompradoPorFornecedorDTO"
+				+ "(f.razaoSocial, sum(i.quantidade * i.valorCompraProduto))"
+				+ "from NotaCompraItem i"
+				+ "join i.notaCompra n"
+				+ "join n.fornecedor f"
+				+ "group by f.razaoSocial";
+		
+		return em.createQuery(query, TotalCompradoPorFornecedorDTO.class).getResultList();
+	}
+	
+}

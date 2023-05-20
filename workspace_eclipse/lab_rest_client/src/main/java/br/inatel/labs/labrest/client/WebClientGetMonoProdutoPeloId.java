bb87450fd5a131b133e9f415804bb9d3aa2bd87f
isadorabello <isadora.bello@gec.inatel.br>
2023-05-20 20:11:17 -0300

    feat: adding mono class

--- workspace_eclipse/lab_rest_client/src/main/java/br/inatel/labs/labrest/client/WebClientGetMonoProdutoPeloId.java
+++ workspace_eclipse/lab_rest_client/src/main/java/br/inatel/labs/labrest/client/WebClientGetMonoProdutoPeloId.java
@@ -1,10 +1,24 @@
 package br.inatel.labs.labrest.client;
 
+import org.springframework.web.reactive.function.client.WebClient;
+
+import br.inatel.labs.labrest.client.model.dto.ProdutoDTO;
+import reactor.core.publisher.Mono;
+
 public class WebClientGetMonoProdutoPeloId {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Mono<ProdutoDTO> monoProduto = WebClient.create("http://localhost:8080").get()
+				.uri("/produto/1").retrieve().bodyToMono(ProdutoDTO.class);
+		
+		monoProduto.subscribe();
+		
+		// é bloqueado a continuação do código até que o registro seja carregado/chegue
+		ProdutoDTO produto = monoProduto.block();
+		
+		System.out.println("Produto encontrado: ");
+		System.out.println(produto);
 	}
 
 }

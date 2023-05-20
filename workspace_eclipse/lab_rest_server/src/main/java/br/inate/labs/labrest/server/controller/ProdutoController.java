5ed7b1a45bd00db23b9cbe9b88ddc110f1f22420
isadorabello <isadora.bello@gec.inatel.br>
2023-05-20 20:03:32 -0300

    feat: adding client side

--- workspace_eclipse/lab_rest_server/src/main/java/br/inate/labs/labrest/server/controller/ProdutoController.java
+++ workspace_eclipse/lab_rest_server/src/main/java/br/inate/labs/labrest/server/controller/ProdutoController.java
@@ -1,11 +1,10 @@
 package br.inate.labs.labrest.server.controller;
 
 import java.util.List;
-import java.util.Optional;
+// import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;

4ed0742ff22e13e0916a93c5a7840dd27532a837
isadorabello <isadora.bello@gec.inatel.br>
2023-05-20 20:16:49 -0300

    feat: adding try/catch to mono side

--- workspace_eclipse/lab_rest_client/src/main/java/br/inatel/labs/labrest/client/WebClientGetMonoProdutoPeloId.java
+++ workspace_eclipse/lab_rest_client/src/main/java/br/inatel/labs/labrest/client/WebClientGetMonoProdutoPeloId.java
@@ -1,6 +1,7 @@
 package br.inatel.labs.labrest.client;
 
 import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.reactive.function.client.WebClientResponseException;
 
 import br.inatel.labs.labrest.client.model.dto.ProdutoDTO;
 import reactor.core.publisher.Mono;
@@ -8,17 +9,24 @@ import reactor.core.publisher.Mono;
 public class WebClientGetMonoProdutoPeloId {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Mono<ProdutoDTO> monoProduto = WebClient.create("http://localhost:8080").get()
-				.uri("/produto/1").retrieve().bodyToMono(ProdutoDTO.class);
-		
-		monoProduto.subscribe();
-		
-		// é bloqueado a continuação do código até que o registro seja carregado/chegue
-		ProdutoDTO produto = monoProduto.block();
-		
-		System.out.println("Produto encontrado: ");
-		System.out.println(produto);
+		try {
+			// TODO Auto-generated method stub
+			Mono<ProdutoDTO> monoProduto = WebClient.create("http://localhost:8080").get()
+					.uri("/produto/1").retrieve().bodyToMono(ProdutoDTO.class);
+			
+			monoProduto.subscribe();
+			
+			// é bloqueado a continuação do código até que o registro seja carregado/chegue
+			ProdutoDTO produto = monoProduto.block();
+			
+			System.out.println("Produto encontrado: ");
+			System.out.println(produto);
+		} catch (WebClientResponseException e) {
+			// TODO Auto-generated catch block
+			System.out.println("Status da resposta: " + e.getStatusCode());
+			System.out.println("Response Body: " + e.getResponseBodyAsString());
+			e.printStackTrace();
+		}
 	}
 
 }

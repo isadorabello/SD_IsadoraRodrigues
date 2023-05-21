56cc8ec823103b10d0acc80e4d308b5a4973a121
isadorabello <isadora.bello@gec.inatel.br>
2023-05-20 21:35:13 -0300

    feat: adding test cases

--- workspace_eclipse/lab_rest_server/src/test/java/br/inate/labs/labrest/server/controller/ProdutoControllerTest.java
+++ workspace_eclipse/lab_rest_server/src/test/java/br/inate/labs/labrest/server/controller/ProdutoControllerTest.java
@@ -1,9 +1,16 @@
 package br.inate.labs.labrest.server.controller;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.reactive.server.WebTestClient;
+
+import br.inate.labs.labrest.server.model.Produto;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class ProdutoControllerTest {
@@ -14,6 +21,27 @@ class ProdutoControllerTest {
 	@Test
 	void deveListarProdutos() {
 		webTestClient.get().uri("/produto").exchange().expectStatus().isOk().expectBody().returnResult();
+	}
+	
+	@Test
+	void givenValidId_whenGetProdutoById_thanResponseWithValidProduto() {
+		Long validId = 1L;
+		
+		Produto produtoValido = webTestClient
+				.get().uri("/produto/"+validId)
+				.exchange().expectStatus().isOk().expectBody(Produto.class).returnResult().getResponseBody();
+		
+		assertNotNull(produtoValido);
+		assertEquals(produtoValido.getId(), validId);
+	}
+	
+	@Test
+	void givenInvalidId_whenGetProdutoById_thanResponseWithInvalidProduto() {
+		Long invalidId = 99L;
+		
+		webTestClient
+				.get().uri("/produto/"+invalidId)
+				.exchange().expectStatus().isNotFound();
 	}
 
 }

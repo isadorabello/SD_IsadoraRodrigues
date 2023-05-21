c9879e1587f1e0d7bc4bf456ac7187c0221a5869
isadorabello <isadora.bello@gec.inatel.br>
2023-05-20 21:03:39 -0300

    feat: adding mono class - put

---
+++ workspace_eclipse/lab_rest_client/src/main/java/br/inatel/labs/labrest/client/WebClientPutProduto.java
@@ -0,0 +1,26 @@
+package br.inatel.labs.labrest.client;
+
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import br.inatel.labs.labrest.client.model.dto.ProdutoDTO;
+
+public class WebClientPutProduto {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ProdutoDTO produtoExistente = new ProdutoDTO();
+		produtoExistente.setId(1L);
+		produtoExistente.setDescricao("Banana Nanica");
+		
+		ResponseEntity<Void> responseEntity = WebClient.create("http://localhost:8080")
+				.put().uri("/produto").bodyValue(produtoExistente).retrieve().toBodilessEntity().block();
+		
+		HttpStatusCode statusCode = responseEntity.getStatusCode();
+		
+		System.out.println("Produto atualizado: ");
+		System.out.println("Status da resposta: " + statusCode);
+	}
+
+}

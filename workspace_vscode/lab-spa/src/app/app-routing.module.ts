358f2f5957843844aa2427e1a22623585d8ad10d
isadorabello <isadora.bello@gec.inatel.br>
2023-06-15 08:58:10 -0300

    adding static infos

--- workspace_vscode/lab-spa/src/app/app-routing.module.ts
+++ workspace_vscode/lab-spa/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProdutoTabelaComponent } from './components/produto-tabela/produto-tabela.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'produto_tabela',
+    component: ProdutoTabelaComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
